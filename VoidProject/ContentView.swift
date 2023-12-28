de605b796aa5435c8dd5182fdfb22fd7604ee5fe
borley1211 <km.isetan@gmail.com>
2023-12-28 15:49:44 +0900

    Initial Commit

---
+++ VoidProject/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  VoidProject
+//
+//  Created by Miebori Kazuma on 2023/12/28.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ VoidProject/VoidProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  VoidProjectApp.swift
+//  VoidProject
+//
+//  Created by Miebori Kazuma on 2023/12/28.
+//
+
+import SwiftUI
+
+@main
+struct VoidProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
